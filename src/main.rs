fcba268ee0e6f2f46c4156e20074b4bd905c1473
Kim.Kitsik <kitsik.kim@gmail.com>
2022-05-03 16:41:01 +0300

    usb otg fs toimib

--- src/main.rs
+++ src/main.rs
@@ -1,162 +1,193 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
-extern crate panic_halt;
 
-use panic_halt as _;
-
-use core::fmt::Write;
-use core::cell::{Cell, RefCell};
-use core::ops::{DerefMut, Not};
-use core::ptr::addr_of_mut;
-use arrayvec::ArrayString;
+use core::sync::atomic::{AtomicU32, Ordering};
 use cortex_m;
-use cortex_m::interrupt::{free, CriticalSection, Mutex};
-use cortex_m_rt::entry;
-use stm32f4xx_hal as hal;
-use stm32f4xx_hal::i2c::Error::Timeout;
-use stm32f4xx_hal::otg_fs::{UsbBus, USB};
-use stm32f4xx_hal::{prelude::*};
-use stm32f4xx_hal::serial::Serial;
-use stm32f4xx_hal::spi::{Mode, Phase, Polarity, Spi};
-use stm32f4xx_hal::time::Hertz;
-use usb_device::bus::UsbBusAllocator;
-use usb_device::prelude::*;
-use usbd_serial::SerialPort;
+use cortex_m::peripheral::SYST;
+use max31855::Error;
+use panic_halt as _;
+use stm32f4xx_hal::rcc::Clocks;
+use max31855::Max31855;
 
-use crate::hal::{
-    gpio::{Edge, Input, PC13},
-    i2c::I2c,
-    interrupt, pac,
-    prelude::*,
-    rcc::{Clocks, Rcc},
-    timer::{CounterUs, Event, Timer},
-};
+pub static TIME: AtomicU32 = AtomicU32::new(0);
+pub const SYS_CK_MHZ: u32 = 144;
+pub const SYST_RELOAD: u32 = (SYS_CK_MHZ * 1000) - 1;
 
-static mut EP_MEMORY: [u32; 1024] = [0; 1024];
-static USB_OTG_FS: Mutex<RefCell<Option<CounterUs<pac::TIM2>>>> = Mutex::new(RefCell::new(None));
-static USB_STATE: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
+#[rtic::app(device = stm32f4xx_hal::pac)]
+mod app {
+    use core::fmt::Write;
+    use core::sync::atomic::Ordering;
 
-static mut SERIAL_PORT: Option<SerialPort<UsbBus<USB>>> = None;
-static mut USB_DEVICE: Option<UsbDevice<UsbBus<USB>>> = None;
-static mut USB_BUS: Option<UsbBusAllocator<UsbBus<USB>>> = None;
+    use arrayvec::ArrayString;
+    use max31855::Unit;
+    use stm32f4xx_hal::gpio::{Alternate, Output};
+    use stm32f4xx_hal::otg_fs::{USB, UsbBus};
+    use stm32f4xx_hal::prelude::*;
+    use stm32f4xx_hal::spi::{Mode, Phase, Polarity, Spi};
+    use usb_device::class_prelude::UsbBusAllocator;
+    use usb_device::prelude::*;
 
-#[entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let rcc = dp.RCC.constrain();
+    use crate::{get_time, SYS_CK_MHZ, systick_init};
 
-    // Set up the system clock
-    let clocks = rcc
-        .cfgr
-        .use_hse(8.MHz())
-        .sysclk(48.MHz())
-        .require_pll48clk()
-        .freeze();
+    static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+    static mut USB_BUS: Option<UsbBusAllocator<UsbBus<USB>>> = None;
 
-    let mut delay = dp.TIM5.delay_us(&clocks);
-
-    let gpioa = dp.GPIOA.split();
-
-    let mut cs1 = gpioa.pa4.into_push_pull_output();
-    let mut cs2 = gpioa.pa8.into_push_pull_output();
-    let mut cs3 = gpioa.pa9.into_push_pull_output();
-    let mut cs4 = gpioa.pa10.into_push_pull_output();
-    let sck = gpioa.pa5.into_alternate();
-    let miso = gpioa.pa6.into_alternate();
-    let mosi = gpioa.pa7.into_alternate();
-
-    let mode = Mode {
-        polarity: Polarity::IdleLow,
-        phase: Phase::CaptureOnFirstTransition,
-    };
-    let mut spi = dp.SPI1.spi((sck, miso, mosi), mode, 1_000_000.Hz(), &clocks);
-
-    cs1.set_high();
-    cs2.set_high();
-    cs3.set_high();
-    cs4.set_high();
-
-    //USB init
-
-    //väike pauss, simuleerib USB restarti.
-    let mut usb_pin_d_plus = gpioa.pa12.into_push_pull_output();
-    usb_pin_d_plus.set_low();
-    delay.delay_ms(100_u32);
-    // Now we can connect as a USB serial device to the host.
-    let usb_pin_d_plus = usb_pin_d_plus.into_alternate();
-    let usb_pin_d_minus = gpioa.pa11.into_alternate();
-
-    let usb = USB {
-        usb_global: dp.OTG_FS_GLOBAL,
-        usb_device: dp.OTG_FS_DEVICE,
-        usb_pwrclk: dp.OTG_FS_PWRCLK,
-        pin_dm: usb_pin_d_minus,
-        pin_dp: usb_pin_d_plus,
-        hclk: clocks.hclk(),
-    };
-
-    unsafe {
-        USB_BUS = Some(UsbBus::new(usb, unsafe { &mut EP_MEMORY }));
-        SERIAL_PORT = Some(usbd_serial::SerialPort::new(USB_BUS.as_ref().unwrap()));
-        USB_DEVICE = Some(UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
-            //.self_powered(true)
-            .manufacturer("Fake company")
-            //.device_release(0x0010)
-            .product("Serial port")
-            .serial_number("TEST")
-            .device_class(usbd_serial::USB_CLASS_CDC)
-            .build());
+    #[shared]
+    struct Shared {
+        serial: usbd_serial::SerialPort<'static, UsbBus<USB>>,
     }
 
-    let mut serial = unsafe { SERIAL_PORT.as_mut().unwrap() };
-    // let mut usb_dev = unsafe { USB_DEVICE.as_mut().unwrap() };
-
-    // Set up the interrupt timer
-    let mut timer = dp.TIM2.counter(&clocks);
-    timer.start(1.secs()).unwrap();
-    timer.listen(Event::Update);
-
-    free(|cs| {
-        USB_OTG_FS.borrow(cs).replace(Some(timer));
-    });
-
-    // Enable interrupt
-    pac::NVIC::unpend(hal::pac::Interrupt::OTG_FS);
-    unsafe {
-        pac::NVIC::unmask(hal::pac::Interrupt::OTG_FS);
+    #[local]
+    struct Local {
+        usb_dev: UsbDevice<'static, UsbBus<USB>>,
+        spi: Spi<stm32f4xx_hal::pac::SPI1, (stm32f4xx_hal::gpio::Pin<'A', 5_u8, Alternate<5_u8>>, stm32f4xx_hal::gpio::Pin<'A', 6_u8, Alternate<5_u8>>, stm32f4xx_hal::gpio::Pin<'A', 7_u8, Alternate<5_u8>>)>,
+        cs1: stm32f4xx_hal::gpio::Pin<'A', 4_u8, Output>,
+        delay: stm32f4xx_hal::timer::Delay<stm32f4xx_hal::pac::TIM5, 1000000_u32>,
     }
 
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let device: stm32f4xx_hal::pac::Peripherals = ctx.device;
+        let _syscfg = device.SYSCFG.constrain();
+        // let reset_flags = device.RCC.csr.read();
+        let rcc = device.RCC.constrain();
 
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.MHz())
+            .sysclk(SYS_CK_MHZ.MHz())
+            .require_pll48clk()
+            .freeze();
 
-    loop {
+        systick_init(&mut ctx.core.SYST, clocks);
+        let gpioa = device.GPIOA.split();
 
-        //termopaari kivi lugemine
-        let mut data = [0; 4];
+        let mut delay = device.TIM5.delay_us(&clocks);
+        let mut cs1 = gpioa.pa4.into_push_pull_output();
+        let mut cs2 = gpioa.pa8.into_push_pull_output();
+        let mut cs3 = gpioa.pa9.into_push_pull_output();
+        let mut cs4 = gpioa.pa10.into_push_pull_output();
+        let sck = gpioa.pa5.into_alternate();
+        let miso = gpioa.pa6.into_alternate();
+        let mosi = gpioa.pa7.into_alternate();
+
+        let mode = Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        };
+        let mut spi = device.SPI1.spi((sck, miso, mosi), mode, 1_000_000.Hz(), &clocks);
+
+        //kõik 4 termopaari kivi vaikimisi välja
         cs1.set_high();
-        delay.delay_ms(1_u32);
-        cs1.set_low();
-        delay.delay_ms(1_u32);
-        spi.transfer(&mut data[..]).unwrap();
-        //delay.delay_ms(1_u32);
+        cs2.set_high();
+        cs3.set_high();
+        cs4.set_high();
 
-        // 200 baidine buffer
-        let mut buf = ArrayString::<200>::new();
-        for x in data {
-            write!(&mut buf, "x= {}\r\n", x).ok();
+        let usb = USB {
+            usb_global: device.OTG_FS_GLOBAL,
+            usb_device: device.OTG_FS_DEVICE,
+            usb_pwrclk: device.OTG_FS_PWRCLK,
+            pin_dm: gpioa.pa11.into_alternate(),
+            pin_dp: gpioa.pa12.into_alternate(),
+            hclk: clocks.hclk(),
+        };
+
+        let usb_bus = UsbBus::new(usb, unsafe { &mut EP_MEMORY });
+
+        let serial: usbd_serial::SerialPort<'static, UsbBus<USB>>;
+        let usb_dev: UsbDevice<'static, UsbBus<USB>>;
+
+        unsafe {
+            USB_BUS = Some(usb_bus);
+            serial = usbd_serial::SerialPort::new(USB_BUS.as_ref().unwrap());
+
+            usb_dev = UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x05dc))
+                //.self_powered(true)
+                .manufacturer("Fake company")
+                //.device_release(0x0010)
+                .product("Serial port")
+                .serial_number("TEST")
+                .device_class(usbd_serial::USB_CLASS_CDC)
+                .build();
         }
-        let y="hello";
-        free(|cs| serial.write(buf.as_bytes()));
-        //free(|cs| serial.write(y.as_bytes()));
+
+        (Shared { serial }, Local { usb_dev, spi, cs1, delay }, init::Monotonics())
     }
 
-    #[interrupt]
-    fn OTG_FS() {
-        let mut serial = unsafe { SERIAL_PORT.as_mut().unwrap() };
-        let mut usb_dev = unsafe { USB_DEVICE.as_mut().unwrap() };
+    #[idle(shared = [serial], local = [spi, cs1, delay])]
+    fn idle(mut ctx: idle::Context) -> ! {
+        let mut next_time = get_time();
 
-        free(|_| {
-            if !usb_dev.poll(&mut [serial]) {}
+        loop {
+            let time = get_time();
+
+            if time > next_time {
+                let mut buf = ArrayString::<200>::new();
+                let mut data = [0u8; 4];
+
+                ctx.local.cs1.set_high();
+                ctx.local.delay.delay_ms(1_u32);
+                ctx.local.cs1.set_low();
+                ctx.local.delay.delay_ms(1_u32);
+
+                ctx.local.spi.transfer(&mut data[..]).unwrap();
+                let x = u32::from_be_bytes(&data[0..4]);
+
+                //write!(&mut buf, "{}\r\n", time);
+                for x in data {
+                    write!(&mut buf, "x= {}\r\n", x).ok();
+                }
+
+                ctx.shared.serial.lock(|serial| {
+                    serial.write(&mut buf.as_bytes()); //.as_bytes()
+                });
+                next_time += 1000000000;
+            }
+
+            ctx.shared.serial.lock(|serial| {
+                let mut buf = [0u8; 64];
+                serial.read(&mut buf);
+            });
+        }
+    }
+
+    #[task(binds = OTG_FS, local = [usb_dev], shared = [serial])]
+    fn otg_fs_event(mut ctx: otg_fs_event::Context) {
+        ctx.shared.serial.lock(|serial: &mut usbd_serial::SerialPort<'static, UsbBus<USB>>| {
+            if !ctx.local.usb_dev.poll(&mut [serial]) {
+                return;
+            }
         });
+    }
+
+    #[task(binds = SysTick, priority = 15)]
+    fn systick_tick(_: systick_tick::Context) {
+        crate::TIME.fetch_add(1, Ordering::Relaxed);
+    }
+}
+
+fn systick_init(syst: &mut SYST, clocks: Clocks) {
+    let c_ck = clocks.sysclk().to_Hz();
+
+    syst.set_clock_source(cortex_m::peripheral::syst::SystClkSource::Core);
+    syst.set_reload((c_ck / 1000) - 1);
+    syst.enable_interrupt();
+    syst.enable_counter();
+}
+
+
+
+
+pub fn get_time() -> u64 {
+    loop {
+        let msec = TIME.load(Ordering::Relaxed);
+        let syst = SYST::get_current();
+        let msec2 = TIME.load(Ordering::Relaxed);
+        if msec == msec2 {
+            let x = (SYST_RELOAD - syst) * (1_000_000 / (SYST_RELOAD + 1));
+
+            return (msec as u64) * 1_000_000 + x as u64;
+        }
     }
 }
